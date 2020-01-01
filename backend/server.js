193eb5c2a97a440f637865a6e026cf21b2320e5d
osamahabib5 <osama.habib@khi.iba.edu.pk>
2020-01-01 15:12:28 +0500

    First commit

---
+++ backend/server.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const todoRoutes = express.Router();
+const PORT = 4000;
+
+let Todo = require('./todo.model');
+
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+//once function is used to handle an event only once
+//connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', function () {
+    console.log("MongoDB database connection established successfully");
+})
+//first endpoint
+todoRoutes.route('/').get(function (req, res) {
+    Todo.find(function (err, todos) {
+        if (err) {
+            console.log(err);
+        } else {   //we are getting the data back from the
+            //mongo db into response object and
+            //passing in todos obj because this is the 
+            //data that we are receiving when accessing
+            //mongodb db based on mongoose model
+            res.json(todos);
+        }
+    });
+});
+
+//we would like to accept incoming HTTP get requests
+//and itneeds to be handled by a callback func
+todoRoutes.route('/:id').get(function (req, res) {
+    let id = req.params.id;   //extract id of incoming
+    //req URL  
+    Todo.findById(id, function (err, todo) {
+        res.json(todo);
+    });
+});
+
+//send an HTTP post req when adding new items to db
+
+todoRoutes.route('/add').post(function (res, req) {
+    //retrieve data from request body
+    let todo = new Todo(req.body);
+    //save to the database
+    todo.save().then(todo => {
+        res.status(200).json({
+            'todo': 'todo added successfuly'
+        });
+        //inserting done properly 200 is HTTTP SUCCESS CODE
+    })
+        .catch(err => {
+            res.status(400).send('adding new todo failed');
+        });
+
+});
+
+//update endpoint
+todoRoutes.route('/update/:id').post(function (req, res) {
+    Todo.findById(req.params.id, function (err, todo) {
+        if (!todo) {
+            res.status(404).send('data is not found');
+        }
+        else {
+            //retrieving data from URL that needs to be update
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+        }
+        //saving in database
+        todo.save().then(todo => {
+            res.json('Todo updated');
+        })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+
+    });
+});
+app.use('/todos', todoRoutes);
+
+//this is used to retrieve the connection
+app.listen(PORT, function () {
+    console.log("Server is runnig on Port: " + PORT);
+});
--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -4,6 +4,11 @@ export default class CreateToDo extends Component {
     constructor(props) {
         super(props);
 
+        this.onchangeTodoDescription = this.onchangeTodoDescription.bind(this);
+        this.onchangeTodoResponsible = this.onchangeTodoResponsible.bind(this);
+        this.onchangeTodoPriority = this.onchangeTodoPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);      //we are binding the following functions to 
+        //this object
         this.state = {
             todo_description: '',
             todo_responsible: '',
@@ -12,17 +17,17 @@ export default class CreateToDo extends Component {
         }
     }
 
-    onChangeTodoDescription(e) {
+    onchangeTodoDescription(e) {            //to update the properties
         this.setState({
             todo_description: e.target.value
         })
     }
-    onChangeTodoResponsible(e) {
+    onchangeTodoResponsible(e) {
         this.setState({
             todo_responsible: e.target.value
         })
     }
-    onChangeTodoPriority(e) {
+    onchangeTodoPriority(e) {
         this.setState({
             todo_priority: e.target.value
         })
@@ -30,12 +35,74 @@ export default class CreateToDo extends Component {
 
     onSubmit(e) {
         e.preventDefault();
+        console.log('Form submitted!');
+        console.log(`Todo Description: ${this.state.todo_description}`);
+        console.log(`Todo responsible: ${this.state.todo_responsible}`);
+        console.log(`Todo Priority: ${this.state.todo_priority}`);
+        console.log(`Todo Completed: ${this.state.todo_completed}`);
         //we are doing this to prevent the default behaviour of the submit button to happen
+        this.setState({     //we are doing this to reset the value once the submit button is clicked
+            todo_description: '',
+            todo_responsible: '',
+            todo_priority: '',
+            todo_completed: false
+        })
     }
     render() {
         return (
-            <div>
-                <p>Welcome to Create to Do</p>
+            <div style={{ marginTop: 20 }}>
+                <h3>Create New Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Description: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.todo_description}
+                            onChange={this.onchangeTodoDescription} /> {/*    the agenda  here is that whenever the input box changes,
+                            todo_description value is updated   */}
+
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input type="text"
+                            className="form-control"
+                            value={this.state.todo_responsible}
+                            onChange={this.onchangeTodoResponsible} /> {/*    the agenda  here is that whenever the input box changes,
+                            todo_description value is updated   */}
+
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input"
+                            type="radio" name="priorityOptions"
+                            id="priorityLow" value="Low"
+                            checked={this.state.todo_priority ===
+                                'Low'}
+                            onChange={this.onchangeTodoPriority} />
+                        <label className="form-check-label">Low</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input"
+                            type="radio" name="priorityOptions"
+                            id="priorityMedium" value="Medium"
+                            checked={this.state.todo_priority ===
+                                'Medium'}
+                            onChange={this.onchangeTodoPriority} />
+                        <label className="form-check-label">Medium</label>
+                    </div>
+                    <div className="form-check form-check-inline">
+                        <input className="form-check-input"
+                            type="radio" name="priorityOptions"
+                            id="priorityHigh" value="High"
+                            checked={this.state.todo_priority ===
+                                'High'}
+                            onChange={this.onchangeTodoPriority} />
+                        <label className="form-check-label">High</label>
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create Todo"
+                            className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
